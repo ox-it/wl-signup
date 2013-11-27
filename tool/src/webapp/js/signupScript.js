878066f12a7a2a89834e85fc483e7eaf08192ceb
Ben Holmes <ben.holmes@it.ox.ac.uk>
2013-11-27 17:28:21 +0000

    WL-3114 Notification checkbox toggles suboptions
    Was hiding the options but now we set the disabled property on each input

--- tool/src/webapp/js/signupScript.js
+++ tool/src/webapp/js/signupScript.js
@@ -463,14 +463,15 @@
 	function isShowEmailChoice(){
 		var emailChoiceTag = document.getElementById('meeting:emailChoice');
 		var emailAttendeeOnlyTag = document.getElementById('meeting:emailAttendeeOnly');
+		var emailAttendeeInputs = emailAttendeeOnlyTag.getElementsByTagName('input');
 
 		if(!emailChoiceTag || !emailAttendeeOnlyTag)
 			return;
 
-		if(emailChoiceTag.checked)
-			emailAttendeeOnlyTag.style.display = "";
-		else
-			emailAttendeeOnlyTag.style.display = "none";
+		var i = 0, input;
+		while (input = emailAttendeeInputs[i++]) {
+			input.disabled = !emailChoiceTag.checked;
+		}
 	}
 
 	function showDTimeInputFields(opt){
