52442c367dc2fef8df4ed206d55969b1b915cd95
Ben Holmes <ben.holmes@it.ox.ac.uk>
2014-01-07 14:02:29 +0000

    SIGNUP-230 : fixed the empty choice dropdown list when it is a new site and no such information available.
    git-svn-id: https://source.sakaiproject.org/contrib/signup/trunk@85289 fdecad78-55fc-0310-b1b2-d7d25cf747c9
    
    Conflicts:
    	tool/src/java/org/sakaiproject/signup/tool/jsf/NewSignupMeetingBean.java

--- tool/src/java/org/sakaiproject/signup/tool/jsf/NewSignupMeetingBean.java
+++ tool/src/java/org/sakaiproject/signup/tool/jsf/NewSignupMeetingBean.java
@@ -2009,12 +2009,14 @@ public class NewSignupMeetingBean implements MeetingTypes, SignupMessageTypes, S
 	
 	public boolean isAllLocationsEmpty(){
 		// this is safe to call often since we cache the locations
-		return Utilities.getSignupMeetingsBean().getAllLocations().isEmpty();
+		//it already has one label item in
+		return !(getAllLocations().size()>1);
+			
 	}
 	
 	public boolean isCategoriesExist() {
 		// this is safe to call often since we cache the categories
-		return !Utilities.getSignupMeetingsBean().getAllCategories().isEmpty();
+		return getAllCategories().size()>1;
 	}
 
 	public UserDefineTimeslotBean getUserDefineTimeslotBean() {
