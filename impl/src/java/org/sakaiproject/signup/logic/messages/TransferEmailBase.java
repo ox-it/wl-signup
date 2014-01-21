b02bfabc938897515304d3939b4128af43934430
Ben Holmes <ben.holmes@it.ox.ac.uk>
2014-01-21 14:53:02 +0000

    WL-3170 Transfer email generates events

--- impl/src/java/org/sakaiproject/signup/logic/messages/TransferEmailBase.java
+++ impl/src/java/org/sakaiproject/signup/logic/messages/TransferEmailBase.java
@@ -6,7 +6,6 @@ import org.sakaiproject.signup.model.SignupTimeslot;
 import org.sakaiproject.user.api.User;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 
@@ -20,53 +19,30 @@ abstract public class TransferEmailBase extends SignupEmailBase implements Signu
      * {@inheritDoc}
      */
     public List<ExtEvent> generateEvents(User user, SignupCalendarHelper calendarHelper) {
-        //NOTE: sent to attendee when organiser moves them to a different timeslot/ swaps them with another user
-        if(logger.isDebugEnabled()){
-            logger.debug("MoveAttendeeEmail/SwapAttendeeEmail");
-        }
 
-        //get new list of events. For all removed cancel them, add the added ones
-        //need to handle this separately for the two diff email object types though
-        List<SignupTimeslot> removed = new ArrayList<SignupTimeslot>();
-        List<SignupTimeslot> added = new ArrayList<SignupTimeslot>();
-        if(email instanceof MoveAttendeeEmail) {
-            removed = ((MoveAttendeeEmail) email).getRemoved();
-            added = ((MoveAttendeeEmail) email).getAdded();
-        } else if (email instanceof SwapAttendeeEmail) {
-            removed = ((SwapAttendeeEmail) email).getRemoved();
-            added = ((SwapAttendeeEmail) email).getAdded();
-        }
-
-        //The tracking classes don't  maintain the transient VEVents we have created previously
+        //The tracking classes don't maintain the transient VEVents we have created previously
         //so we need to check and recreate.
 
         //cancel all of the removed events
-        List<ExtEvent> vevents = new ArrayList<ExtEvent>();
-        for(SignupTimeslot ts: removed) {
-
+        List<ExtEvent> events = new ArrayList<ExtEvent>();
+        for(SignupTimeslot timeslot: this.getRemoved()) {
             //check and recreate if necessary
-            ExtEvent v = ensureExtEventForTimeslot(meeting, ts);
-
-            if(v != null){
-                //set it to be cancelled, add to list
-                vevents.add(calendarHelper.cancelExtEvent(v));
+            ExtEvent event = calendarHelper.generateExtEventForTimeslot(meeting, timeslot);
+            if(event != null){
+                calendarHelper.cancelExtEvent(event);
+                events.add(event);
             }
         }
 
         //add all of the new events
-        for(SignupTimeslot ts: added) {
-
+        for(SignupTimeslot timeslot: this.getAdded()) {
             //check and recreate if necessary
-            ExtEvent v = ensureExtEventForTimeslot(meeting, ts);
-
-            if(v != null){
-                vevents.add(v);
+            ExtEvent event = calendarHelper.generateExtEventForTimeslot(meeting, timeslot);
+            if(event != null){
+                events.add(event);
             }
         }
-
-        //create calendar and final attachment, if we have vevents to work with
-        if(vevents.size()>0){
-            attachments.add(formatICSAttachment(vevents, "REQUEST"));
-        }
+        return events;
     }
+
 }
